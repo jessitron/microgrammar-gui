7b5840bbe69357c4663265f54bc8f192dadd3dcf
Jessica Kerr <jessitron@atomist.com>
2019-04-13 21:08:13 -0500

    The code window exists

--- app/definition/MicrogrammarPhrase.tsx
+++ app/definition/MicrogrammarPhrase.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { State } from '../State';
-import CodeMirror from 'codemirror';
+import { Controlled as CodeMirror } from "react-codemirror2";
 import { ErrorBoundary } from '../ErrorBoundary';
 
 export const MicrogrammarPhrase: React.FC<State> = () => {
     return <div className="mg-phrase">
         <label>Microgrammar phrase:</label>
         <ErrorBoundary>
-            <CodeMirror
-                value=""
-                options={{}}
+            <CodeMirror /* TODO: bring in a theme because it doesn't line itself up */
+                className="codemirror"
+                value="bananas" /* IMPORTANT: pass the state around. */
+                options={{ lineNumbers: true }}
                 onBeforeChange={(editor: any, data: any, value: any) => {
                     // this.setState({ value });
                 }}
