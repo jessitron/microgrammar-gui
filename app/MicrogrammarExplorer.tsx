0b3a8cffe7f66c0b0f598339506db6e2f4b44fc6
Jessica Kerr <jessitron@atomist.com>
2019-04-13 19:09:48 -0500

    Start an app

---
+++ app/MicrogrammarExplorer.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { State } from './State';
+
+export const MicrogrammarExplorer: React.FC<State> = () => {
+    return <div>Yay</div>;
+}
---
+++ app/app.tsx
@@ -0,0 +1,7 @@
+import * as ReactDOM from "react-dom";
+import React from "react";
+import { MicrogrammarExplorer } from "./MicrogrammarExplorer";
+
+ReactDOM.render(
+    <MicrogrammarExplorer />,
+    document.getElementById("main"));
